89fc2c06275da94cb8cd3b546b5bc3a9e8adb82c
Bairon2002 <56143846+Bairon2002@users.noreply.github.com>
2021-12-21 14:29:47 -0500

    Delete co.com.Utest directory

--- co.com.Utest/src/main/java/co/com/Utest/userinterface/UtestPage.java
+++
@@ -1,8 +0,0 @@
-package co.com.Utest.userinterface;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("https://www.utest.com/signup/personal")
-public class UtestPage extends PageObject {
-}
